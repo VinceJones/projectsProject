3d1bc8a580d7e324baf8d1ad8baba5602872b904
Vince Jones <vincethebutcher@gmail.com>
2015-05-07 14:40:28 -0500

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+    //Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n'
+            },
+            build: {
+                src: 'client/app.js',
+                dest: 'server/public/assets/scripts/app.min.js'
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "node_modules/bootstrap",
+                        src: [
+                            "dist/css/*",
+                            "dist/fonts/*",
+                            "dist/js/*",
+                            "fonts/*"
+                        ],
+                        "dest": "server/public/vendors/bootstrap"
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/",
+                        src: [
+                            "bootstrap/fonts/*"
+                        ],
+                        "dest": "server/public/vendors"
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/",
+                        src: [
+                            "jquery/dist/jquery.min.js",
+                            "jquery/dist/jquery.min.map"
+                        ],
+                        "dest": "server/public/vendors/jquery"
+                    },
+                    {
+                        expand: true,
+                        cwd: "client/",
+                        src: [
+                            "styles.css"
+                        ],
+                        "dest": "server/public/assets/styles"
+                    }
+                ]
+            }
+        }
+
+
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //Default task(s)
+    grunt.registerTask('default', ['copy', 'uglify']);
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+#Project Website
+
+##Overview
+This website will be about different projects that I have done and am in the process of doing. There will be some
+pictures and also some descriptions of these projects.
+
+###Projects
+
+####Apps to build
+    * D&D character builder app
+
+####1977 Chevy c10 Pick-up
+    * Get my truck to run
+
+####Anoraks
+    * Build an inventory of anoraks
+
+####Camping
+    * Pictures of trips
+
+####Cooking
+    * this will contain things that I have cooked over the years
